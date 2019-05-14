14b4fd60163b9c1a0123533b01820e0e193313ef
corast <corastweb94@hotmail.com>
2019-05-14 15:55:24 +0200

    moved dataset information to information 2

--- main_test.py
+++ main_test.py
@@ -629,12 +629,38 @@ def load_model():
     # v1: { "precision": [a,b,...,n], "coverage": [a,b,...,n], "feature":[0,1,...,f], "names":[n_1,n_2,...,n_f] }
     # v2: {exp1:[None,None,2,4], exp2: [None,None,5,2,5] }
 
+def dataset_info_2():
+    import sklearn
+    import numpy as np
+    from sklearn import model_selection
+    from DNN.kera import pre_processing
+    
+    datamanager = pre_processing.Datamanager(dataset="adults",in_mod="normal",out_mod="normal")
+    dataset = datamanager.ret
+    print(dataset.__dict__.keys())
+
+    print(dataset.df.groupby('income').agg(['count','nunique']).stack())
+    print(dataset.df.groupby('age').agg(['count','nunique']).stack())
+    print(dataset.df.groupby('education').agg(['count','nunique']).stack())
+    print()
+    print(dataset.df.groupby('sex').agg({'income':['count','nunique']}).stack())
+    print()
+    print(dataset.df.groupby('sex').agg({'income':['count','nunique']}))
+
+    g_sum = dataset.df.groupby('education')['income'].transform('sum')
+    print(g_sum)
+    values = dataset.df['income']/g_sum
+    print(values)
+    df['Entropy'] = -(values*np.log(values))
+
+    df1 = df.groupby('Name_Receive',as_index=False,sort=False)['Entropy'].sum()
+    exit()
 
 def dataset_info():
     import sklearn
     import numpy as np
     from sklearn import model_selection
-    from DNN.keras import pre_processing
+    from DNN.kera import pre_processing
     
     datamanager = pre_processing.Datamanager(dataset="adults",in_mod="normal",out_mod="normal")
     dataset = datamanager.ret
@@ -781,5 +807,5 @@ def complete_test():
 #dataset_info()
 #load_model()
 #dataset_info()
-
-complete_test()
+dataset_info_2()
+#complete_test()
